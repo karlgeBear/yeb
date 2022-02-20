a74ffeeab7e6690de23fe7407e259559a53c52b0
zengxinfu <karlge_bear@163.com>
2022-02-20 21:47:55 +0800

    First commit

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      Login
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() { 
+    return {
+
+    }
+  },
+  components:{
+  },
+  methods:{
+
+  },
+  mounted() {
+
+  },
+ }
+</script>
+
+<style>
+</style>
